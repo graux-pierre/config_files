2b5dbd54c6ca7de1cddc6b2ef94c081ad6742c22
pat le panda <patlepanda@Panda-Tower.lan>
2015-02-08 19:45:45 +0100

    xmonad - xmobar - gdb

---
+++ xmonad.hs
@@ -0,0 +1,50 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Actions.CycleWS
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "xmobar /home/patlepanda/.xmobarrc"
+
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+	} 
+
+	  `additionalKeys`
+        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+
+	-- xterm shortcut
+	, ((mod4Mask, xK_t), spawn "xterm")
+	, ((mod4Mask, xK_T), spawn "xterm")
+	
+	-- firefox shortcut
+	, ((mod4Mask, xK_f), spawn "firefox")
+	, ((mod4Mask, xK_F), spawn "firefox")
+
+	-- se left and right (+shift) to switch beetween WS
+        , ((mod4Mask, xK_Right), nextWS)
+	, ((mod4Mask, xK_Left), prevWS)
+	, ((mod4Mask .|. shiftMask, xK_Right), shiftToNext >> nextWS)
+	, ((mod4Mask .|. shiftMask, xK_Left), shiftToPrev >> prevWS)
+        
+
+	-- sound keys
+   	--
+   	-- XF86AudioLowerVolume
+   	, ((0            , 0x1008ff11), spawn "amixer -c 0 set Master 2dB-")
+   	-- XF86AudioRaiseVolume
+   	, ((0            , 0x1008ff13), spawn "amixer -c 0 set Master 2dB+")
+   	-- XF86AudioMute
+   	, ((0            , 0x1008ff12), spawn "amixer -D pulse set Master toggle")
+	]
+
